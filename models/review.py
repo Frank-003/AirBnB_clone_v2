561d7deab7632af2e02eedb381aef16a0f96c8c2
Frank-003 <oyiengofrankline49@gmail.com>
2024-04-20 06:34:23 -0700

    task 9

---
+++ models/review.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Review module for the HBNB project """
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """ Review classto store review information """
+    place_id = "00400"
+    user_id = "40163748"
+    text = ""
