d2eded1d326b80262457494ce7e31ab3f93d7e47
Frank-003 <oyiengofrankline49@gmail.com>
2024-05-18 11:38:06 +0000

    task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,22 +3,23 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 
-# Set up SQLAlchemy session
-@app.teardown_appcontext
-def close_db(error):
-    storage.close()
 
-# Route to display a list of states
-@app.route('/states_list', strict_slashes=False)
+@app.teardown_appcontext
+def teardown_db(exception):
+    """ teardown db"""
+    if storage is not None:
+        storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
 def states_list():
-    # Get all State objects from DBStorage and sort by name
-    states = sorted(storage.all(State).values(), key=lambda s: s.name)
-    return render_template('states_list.html', states=states)
+    """ list of state ids"""
+    data = storage.all(State)
+    return render_template('7-states_list.html', total=data.values())
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
