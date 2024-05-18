1d08277c5f8fe5d15a07ecb982cc3c7949627a11
Frank-003 <oyiengofrankline49@gmail.com>
2024-05-18 12:23:40 +0000

    task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python3
-""" 7. Start flask service that does something. """
-
-from flask import Flask
-from flask import render_template
-
+"""Flask framework"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-#@app.route()
+@app.teardown_appcontext
+def teardown_db(exception):
+    """ teardown db"""
+    if storage is not None:
+        storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """ list of state ids"""
+    data = storage.all(State)
+    return render_template('7-states_list.html', total=data.values())
 
 
 if __name__ == '__main__':
